33a38ad3b6ac6251be7a8ebe71d98c90cb927854
hanumasuseela <poomdlasuseela100@gmail.com>
2024-06-22 18:39:00 -0700

    New Methods
    New Methods

--- src/test/java/com/luv2code/suseela/junitdemo/DemoUnitsTest.java
+++ src/test/java/com/luv2code/suseela/junitdemo/DemoUnitsTest.java
@@ -3,8 +3,7 @@ package com.luv2code.suseela.junitdemo;
 import com.luv2code.junitdemo.DemoUtils;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.*;
 
 public class DemoUnitsTest {
 
@@ -13,5 +12,15 @@ public class DemoUnitsTest {
          DemoUtils demoUtils = new DemoUtils();
          assertEquals("2+4 must be 6",6,demoUtils.add(2,4));
          assertNotEquals("2+4 must not be 7",7,demoUtils.add(2,4));
+     }
+
+     public void testNullandNotNull(){
+         DemoUtils demoutils = new DemoUtils();
+
+         String str1 = null;
+         String str2 = "luv2code";
+
+         assertNull("Demo should  be null",demoutils.checkNull(str1));
+         assertNotNull("Demo should not be null",demoutils.checkNull(str2));
      }
 }
