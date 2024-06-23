0d0bd5e09448f786c8ebb12d1bb874cb984f70e0
hanumasuseela <poomdlasuseela100@gmail.com>
2024-06-22 18:30:50 -0700

    MyJunitTestCourseProject1

--- src/test/java/com/luv2code/suseela/junitdemo/DemoUnitsTest.java
+++ src/test/java/com/luv2code/suseela/junitdemo/DemoUnitsTest.java
@@ -11,7 +11,7 @@ public class DemoUnitsTest {
      @Test
      public void testEqualsandNotEquals(){
          DemoUtils demoUtils = new DemoUtils();
-
+         assertEquals("2+4 must be 6",6,demoUtils.add(2,4));
          assertNotEquals("2+4 must not be 7",7,demoUtils.add(2,4));
      }
 }
